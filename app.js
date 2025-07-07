409d520ee2d8b691dd615b6700be80bd11b1585f
Abdulrasheed313 <rasheedmumna@gmail.com>
2025-07-07 05:52:40 +0500

    second

--- app.js
+++ app.js
@@ -1,49 +1,43 @@
+let getDiv = document.querySelector("#main");
 
-
-let getDiv = document.querySelector('#main')
-
-
-function add(){
-let getSearch = document.querySelector('#search');
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${getSearch.value}&units=metric&appid=2f0ab7dd569278ced30554e7a97314db`)
-.then(data => data.json())
-.then((data)=>{
-      console.log(data)
-            getDiv.innerHTML +=`<div class="card rounded mt-3" style="width: 18rem;">
+function add() {
+  let getSearch = document.querySelector("#search");
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${getSearch.value}&units=metric&appid=2f0ab7dd569278ced30554e7a97314db`
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      console.log(data);
+      getDiv.innerHTML += `<div class="card rounded mt-3" style="width: 18rem;">
   <img src="./cloud.png" class="card-img-top" alt="..." width=100px>
   <div class="card-body">
   <h5 class="card-title">${data.name}</h5>
-  <p class="card-text">Temp: ${data.main.temp}°C</p>
+  <p class="card-text"><span>Temp</span> ${data.main.temp}°C</p>
   <div class="row">
   
   <div class="col-md-6">
-  <p class="card-text">Feel: ${data.main.feels_like}°C</p>
+  <p class="card-text"><span>Feel</span> ${data.main.feels_like}°C</p>
               </div>
               <div class="col-md-6">
-                <p class="card-text">Humidity: ${data.main.humidity}%</p>
+                <p class="card-text"><span>Humidity</span> ${
+                  data.main.humidity
+                }%</p>
               </div>
               <div class="col-md-6">
-                <p class="card-text">Sunrise: ${new Date(data.sys.sunrise * 1000).toLocaleTimeString()}</p>
+                <p class="card-text"><span>Sunrise</span> ${new Date(
+                  data.sys.sunrise * 1000
+                ).toLocaleTimeString()}</p>
               </div>
               <div class="col-md-6">
-                <p class="card-text">Sunset: ${new Date(data.sys.sunset * 1000).toLocaleTimeString()}</p>
+                <p class="card-text"><span>Sunset</span> ${new Date(
+                  data.sys.sunset * 1000
+                ).toLocaleTimeString()}</p>
               </div>
             </div>
-    
-   
-   
-    
-    
-   
   </div>
-</div>`
-        })
-  
+</div>`;
+    })
 
-.catch(err => console.log(err))
-getSearch.value = '';
+    .catch((err) => console.log(err));
+  getSearch.value = "";
 }
-
-
-
-
